60bd236587fda7a22aec74cee05d81049d412054
your_github_username <your_github_email>
2023-12-03 09:32:27 +0530

    added logic how to generate password and also how to reset

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import * as Yup from 'yup';
 
@@ -9,7 +9,57 @@ const passwordSchema = Yup.object().shape({
     .required('Length is required'),
 });
 
-function App(): JSX.Element {
+function App() {
+  const [password, setPassword] = useState('');
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false);
+  const [lowercase, setLowercase] = useState(false);
+  const [uppercase, setUppercase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePassword = (passwordLength: number) => {
+    let charecterList = '';
+    const uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChar = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const symboolChars = '@#$_&';
+    if (uppercase) {
+      charecterList += uppercaseChar;
+    }
+    if (lowercase) {
+      charecterList += lowercaseChar;
+    }
+    if (numbers) {
+      charecterList += digitChars;
+    }
+    if (symbols) {
+      charecterList += symboolChars;
+    }
+
+    const resultPassword = createPassword(charecterList, passwordLength);
+    setPassword(resultPassword);
+    setIsPasswordGenerated(true);
+  };
+
+  const createPassword = (cheracters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const charIndex = Math.round(Math.random() * cheracters.length);
+      result += cheracters.charAt(charIndex);
+    }
+
+    return result;
+  };
+
+  const resetPassword = () => {
+    setPassword('');
+    setIsPasswordGenerated(false);
+    setLowercase(false);
+    setUppercase(false);
+    setNumbers(false);
+    setSymbols(false);
+  };
+
   return (
     <SafeAreaView>
       <View>
